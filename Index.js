703b82a273b141571e4389288e268870019a9676
Jamal <mybusineess@gmail.com>
2026-03-12 04:10:35 +0000

    if Condition for our game

--- Index.js
+++ Index.js
@@ -1,30 +1,13 @@
-// let firstCard = 10;
-// let secondCard = 11;
-// let sum = firstCard + secondCard;
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
 
-// if (sum < 21) {
-//     console.log("Do you want to draw a new Card! 🙂");
-// } else if (sum === 21) {
-//     console.log("Wohoo! You've got blackjack! 🥳");
-// } else {
-//     console.log("You're out of the game!😭")
-// }
-
-
-// const age = 24
-
-// if (age <= 20) {
-//     console.log("You can not enter the club!")
-
-// } else {
-//     console.log("Welcome!")
-// }
-
-let age = 100
-if (age < 100) {
-    console.log("Not eligible")
-} else if (age === 100) {
-    console.log("Here is your birthday card from the King ")
+if (sum <= 20) {
+    console.log("Do you want to draw a new Card! 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got blackjack! 🥳");
 } else {
-    console.log("Not eligible, you have already gotten one")
+    console.log("You're out of the game!😭")
 }
+
+

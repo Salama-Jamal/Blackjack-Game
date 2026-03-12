5d5074a1ceb4406329788eb21f73d1f8c6da3139
Jamal <mybusineess@gmail.com>
2026-03-12 03:21:56 +0000

    if...else Condition

--- Index.js
+++ Index.js
@@ -1,4 +1,11 @@
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
-console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want to draw a new Card! 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got blackjack! 🥳");
+} else {
+    console.log("You're out of the game!😭")
+}

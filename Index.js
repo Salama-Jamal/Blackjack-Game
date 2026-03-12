d39d2e4057c26343dd7f4e3c2905901042653e36
Jamal <mybusineess@gmail.com>
2026-03-12 05:58:27 +0000

    Add the isAlive variable

--- Index.js
+++ Index.js
@@ -1,26 +1,30 @@
-// let firstCard = 10;
-// let secondCard = 11;
-// let sum = firstCard + secondCard;
-
-// if (sum <= 20) {
-//     console.log("Do you want to draw a new Card! 🙂");
-// } else if (sum === 21) {
-//     console.log("Wohoo! You've got blackjack! 🥳");
-// } else {
-//     console.log("You're out of the game!😭")
-// }
-
-
-
-let hasDiscountCode = true;
-
-function processOrder() {
-    if (hasDiscountCode) {
-        console.log(" Discount applied to order food ")
-        hasDiscountCode = false;
-    } else {
-        console.log("Discount not applied!")
-    }
+let firstCard = 11;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackjack = false
+let isAlive = true
+if (sum <= 20) {
+    console.log("Do you want to draw a new Card! 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got blackjack! 🥳");
+    hasBlackjack = true
+} else {
+    console.log("You're out of the game!😭")
+    isAlive = false;
 }
-processOrder()
-processOrder()
+
+console.log(hasBlackjack)
+console.log(isAlive)
+
+// let hasDiscountCode = true;
+
+// function processOrder() {
+//     if (hasDiscountCode) {
+//         console.log(" Discount applied to order food ")
+//         hasDiscountCode = false;
+//     } else {
+//         console.log("Discount not applied!")
+//     }
+// }
+// processOrder()
+// processOrder()

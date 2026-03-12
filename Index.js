04b19b68242179bb37e6eb706680c8a4ef398536
Jamal <mybusineess@gmail.com>
2026-03-12 06:06:17 +0000

    practice boolean conditions

--- Index.js
+++ Index.js
@@ -1,20 +1,20 @@
-let firstCard = 11;
-let secondCard = 11;
-let sum = firstCard + secondCard;
-let hasBlackjack = false
-let isAlive = true
-if (sum <= 20) {
-    console.log("Do you want to draw a new Card! 🙂");
-} else if (sum === 21) {
-    console.log("Wohoo! You've got blackjack! 🥳");
-    hasBlackjack = true
-} else {
-    console.log("You're out of the game!😭")
-    isAlive = false;
-}
+// let firstCard = 11;
+// let secondCard = 11;
+// let sum = firstCard + secondCard;
+// let hasBlackjack = false
+// let isAlive = true
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new Card! 🙂");
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got blackjack! 🥳");
+//     hasBlackjack = true
+// } else {
+//     console.log("You're out of the game!😭")
+//     isAlive = false;
+// }
 
-console.log(hasBlackjack)
-console.log(isAlive)
+// console.log(hasBlackjack)
+// console.log(isAlive)
 
 // let hasDiscountCode = true;
 
@@ -28,3 +28,12 @@ console.log(isAlive)
 // }
 // processOrder()
 // processOrder()
+
+
+console.log(4 === 3)  // false 
+console.log(5 > 2)    // true
+console.log(12 > 12)  // false 
+console.log(3 < 0)    // false
+console.log(3 >= 3)   //  true
+console.log(11 <= 11) // true
+console.log(3 <= 2)   //false

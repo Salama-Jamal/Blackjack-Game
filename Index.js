a91630ca45f6ccd0b19d775f282cdd5e51680052
Jamal <mybusineess@gmail.com>
2026-03-12 03:39:15 +0000

    elegible age !

--- Index.js
+++ Index.js
@@ -1,11 +1,21 @@
-let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard;
+// let firstCard = 10;
+// let secondCard = 11;
+// let sum = firstCard + secondCard;
 
-if (sum < 21) {
-    console.log("Do you want to draw a new Card! 🙂");
-} else if (sum === 21) {
-    console.log("Wohoo! You've got blackjack! 🥳");
+// if (sum < 21) {
+//     console.log("Do you want to draw a new Card! 🙂");
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got blackjack! 🥳");
+// } else {
+//     console.log("You're out of the game!😭")
+// }
+
+
+const age = 24
+
+if (age <= 20) {
+    console.log("You can not enter the club!")
+
 } else {
-    console.log("You're out of the game!😭")
+    console.log("Welcome!")
 }

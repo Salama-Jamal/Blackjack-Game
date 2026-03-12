59840150f5383e61655b43fe196cfc5571c91774
Jamal <mybusineess@gmail.com>
2026-03-12 04:31:21 +0000

    aside: Boolean

--- Index.js
+++ Index.js
@@ -1,13 +1,26 @@
-let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard;
+// let firstCard = 10;
+// let secondCard = 11;
+// let sum = firstCard + secondCard;
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new Card! 🙂");
-} else if (sum === 21) {
-    console.log("Wohoo! You've got blackjack! 🥳");
-} else {
-    console.log("You're out of the game!😭")
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new Card! 🙂");
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got blackjack! 🥳");
+// } else {
+//     console.log("You're out of the game!😭")
+// }
+
+
+
+let hasDiscountCode = true;
+
+function processOrder() {
+    if ("") {
+        console.log(" Discount applied to order food ")
+        hasDiscountCode = false;
+    } else {
+        console.log("Discount not applied!")
+    }
 }
-
-
+processOrder()
+processOrder()

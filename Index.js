59084898d9854a39f9478043f68d0415950ad3a6
Jamal <mybusineess@gmail.com>
2026-03-14 03:28:13 +0000

    create a empty message to save result after run the function

--- Index.js
+++ Index.js
@@ -1,20 +1,30 @@
-// let firstCard = 11;
-// let secondCard = 11;
-// let sum = firstCard + secondCard;
-// let hasBlackjack = false
-// let isAlive = true
-// if (sum <= 20) {
-//     console.log("Do you want to draw a new Card! 🙂");
-// } else if (sum === 21) {
-//     console.log("Wohoo! You've got blackjack! 🥳");
-//     hasBlackjack = true
-// } else {
-//     console.log("You're out of the game!😭")
-//     isAlive = false;
-// }
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackjack = false
+let isAlive = true
+let message = ""
 
-// console.log(hasBlackjack)
-// console.log(isAlive)
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new Card! 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got blackjack! 🥳"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!😭"
+        isAlive = false;
+    }
+
+}
+startGame()
+
+console.log(message)
+console.log(hasBlackjack)
+console.log(isAlive)
+
+
 
 // let hasDiscountCode = true;
 
@@ -30,10 +40,10 @@
 // processOrder()
 
 
-console.log(4 === 3)  // false 
-console.log(5 > 2)    // true
-console.log(12 > 12)  // false 
-console.log(3 < 0)    // false
-console.log(3 >= 3)   //  true
-console.log(11 <= 11) // true
-console.log(3 <= 2)   //false
+// console.log(4 === 3)  // false 
+// console.log(5 > 2)    // true
+// console.log(12 > 12)  // false 
+// console.log(3 < 0)    // false
+// console.log(3 >= 3)   //  true
+// console.log(11 <= 11) // true
+// console.log(3 <= 2)   //false
